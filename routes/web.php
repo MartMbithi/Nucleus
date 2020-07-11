6b2cb3154d9782fb0f6ee0f399a1ce8b6444fa16
martin <martdevelopers254@gmail.com>
2020-07-12 00:15:38 +0300

    Implemented Features page and its route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/nucleus-erp-features', function ()
+{
+    return view('features');
+});
