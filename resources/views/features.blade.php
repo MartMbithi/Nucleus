7d646d8d7c8514375b1347c08f76bf975ac874ac
martin <martdevelopers254@gmail.com>
2020-07-13 11:55:57 +0300

    Refactored Features page

--- resources/views/features.blade.php
+++ resources/views/features.blade.php
@@ -9,77 +9,77 @@
       <div class="container text-left align-self-center">
         <nav aria-label="breadcrumb">
           <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="{{ url('/') }}">Home</a>
+            <li class="breadcrumb-item"><a href="{{ url('/') }}">{{ __('Home') }}</a>
             </li>
-            <li class="breadcrumb-item"><a href="{{ url('/nucleus-erp-features') }}">Features</a>
+            <li class="breadcrumb-item"><a href="{{ url('/nucleus-erp-features') }}">{{ __('Features') }}</a>
             </li>
           </ol>
         </nav>
-        <h1 class="page-title-heading">Nucleus ERP Features</h1>
-        <div class="page-title-subheading">Some of the features that are <strong>gonna blow your mind off </strong></div>
+        <h1 class="page-title-heading">{{ __('Nucleus ERP Features') }}</h1>
+        <div class="page-title-subheading">{{ __('Some of the features that are') }} <strong>{{ __('gonna blow your mind off') }}</strong></div>
       </div>
     </div>
     <!-- Page Content-->
     <section class="container mt-2 pt-4 pb-5">
       <div class="row pt-4">
         <div class="col-lg-4 col-sm-6 mb-30"><a class="d-block bg-right-center bg-no-repeat bg-white border box-shadow p-5 text-decoration-none"  style="background-image: url(img/homepages/theme-presentation/portfolio.png);">
-            <h3 class="h5">Accounting</h3>
-            <p class="text-muted pb-2">Completely Free Trial. No setup fees. No hidden charges under free trial. Not a free limited version. Free means free..</p>
+            <h3 class="h5">{{ __('Accounting') }}</h3>
+            <p class="text-muted pb-2">{{ __('Completely Free Trial. No setup fees. No hidden charges under free trial. Not a free limited version. Free means free') }}</p>
             </a></div>
 
         <div class="col-lg-4 col-sm-6 mb-30"><a class="d-block bg-right-center bg-no-repeat bg-white border box-shadow p-5 text-decoration-none"  style="background-image: url(img/homepages/theme-presentation/blog.png);">
-            <h3 class="h5">Inventory </h3>
-            <p class="text-muted pb-2">Enable inventory tracking and manage goods as they come in and go out. Items also speed up invoicing.</p>
+            <h3 class="h5"> {{ __('Inventory') }} </h3>
+            <p class="text-muted pb-2">{{ __('Enable inventory tracking and manage goods as they come in and go out. Items also speed up invoicing.') }}</p>
             </a></div>
 
         <div class="col-lg-4 col-sm-6 mb-30"><a class="d-block bg-right-center bg-no-repeat bg-white border box-shadow p-5 text-decoration-none"  style="background-image: url(img/homepages/theme-presentation/shop.png);">
-            <h3 class="h5">Multi-Company</h3>
-            <p class="text-muted pb-2">Manage the finances of multiple companies from one admin panel. Assign users to different companies.</p>
+            <h3 class="h5">{{ __('Multi-Company') }}</h3>
+            <p class="text-muted pb-2">{{ __('Manage the finances of multiple companies from one admin panel. Assign users to different companies.') }}</p>
             </a></div>
 
         <div class="col-lg-4 col-sm-6 mb-30"><a class="d-block bg-right-center bg-no-repeat bg-white border box-shadow p-5 text-decoration-none"  style="background-image: url(img/homepages/theme-presentation/account.png);">
-            <h3 class="h5">Easy Invoicing</h3>
-            <p class="text-muted pb-2">Send professional invoices to clients and start accepting online payments, no commission/transaction fee.</p>
+            <h3 class="h5">{{ __('Easy Invoicing') }}</h3>
+            <p class="text-muted pb-2">{{ __('Send professional invoices to clients and start accepting online payments, no commission/transaction fee.') }}</p>
             </a></div>
 
         <div class="col-lg-4 col-sm-6 mb-30"><a class="d-block bg-right-center bg-no-repeat bg-white border box-shadow p-5 text-decoration-none"  style="background-image: url(img/homepages/theme-presentation/pages.png);">
-            <h3 class="h5">Multi-Currency</h3>
-            <p class="text-muted pb-2">Send invoices and add expenses in any currency and let the system convert them in your main currency.</p>
+            <h3 class="h5">{{ __('Multi-Currency') }}</h3>
+            <p class="text-muted pb-2">{{ __('Send invoices and add expenses in any currency and let the system convert them in your main currency.') }}</p>
             </a></div>
 
             <div class="col-lg-4 col-sm-6 mb-30"><a class="d-block bg-right-center bg-no-repeat bg-white border box-shadow p-5 text-decoration-none"  style="background-image: url(img/homepages/theme-presentation/docs.png);">
-            <h3 class="h5">Billable Expenses</h3>
-            <p class="text-muted pb-2">Create and manage bills so your finances are always accurate and healthy. Know what and when to pay.</p>
+            <h3 class="h5">{{ __('Billable Expenses') }}</h3>
+            <p class="text-muted pb-2">{{ __('Create and manage bills so your finances are always accurate and healthy. Know what and when to pay.') }}</p>
             </a></div>
 
             <div class="col-lg-4 col-sm-6 mb-30"><a class="d-block bg-right-center bg-no-repeat bg-white border box-shadow p-5 text-decoration-none"  style="background-image: url(img/homepages/theme-presentation/docs.png);">
-            <h3 class="h5">Powerful Reporting</h3>
-            <p class="text-muted pb-2">Get detailed financial reports to help you better visualize all the information you need to improve your business.</p>
+            <h3 class="h5">{{ __('Powerful Reporting') }}</h3>
+            <p class="text-muted pb-2">{{ __('Get detailed financial reports to help you better visualize all the information you need to improve your business.') }}</p>
             </a></div>
 
             <div class="col-lg-4 col-sm-6 mb-30"><a class="d-block bg-right-center bg-no-repeat bg-white border box-shadow p-5 text-decoration-none"  style="background-image: url(img/homepages/theme-presentation/account.png);">
-            <h3 class="h5">Client Portal</h3>
-            <p class="text-muted pb-2">Share the transactions and invoices with your clients and accept bulk payments, online.</p>
+            <h3 class="h5">{{ __('Client Portal') }}</h3>
+            <p class="text-muted pb-2">{{ __('Share the transactions and invoices with your clients and accept bulk payments, online.' ) }}</p>
             </a></div>
 
             <div class="col-lg-4 col-sm-6 mb-30"><a class="d-block bg-right-center bg-no-repeat bg-white border box-shadow p-5 text-decoration-none"  style="background-image: url(img/homepages/theme-presentation/portfolio.png);">
-            <h3 class="h5">Customer Management</h3>
-            <p class="text-muted pb-2">Create clients and send invoices to them. </p>
+            <h3 class="h5">{{ __('Customer Management') }}</h3>
+            <p class="text-muted pb-2">{{ __('Create clients and send invoices to them.') }}</p>
             </a></div>
 
             <div class="col-lg-4 col-sm-6 mb-30"><a class="d-block bg-right-center bg-no-repeat bg-white border box-shadow p-5 text-decoration-none"  style="background-image: url(img/homepages/theme-presentation/shop.png);">
-            <h3 class="h5">Vendor Management</h3>
-            <p class="text-muted pb-2">Create vendors so you could assign bills and payments to them and later filter their transactions easily.</p>
+            <h3 class="h5">{{ __('Vendor Management') }}</h3>
+            <p class="text-muted pb-2">{{ __('Create vendors so you could assign bills and payments to them and later filter their transactions easily.') }}</p>
             </a></div>
 
             <div class="col-lg-4 col-sm-6 mb-30"><a class="d-block bg-right-center bg-no-repeat bg-white border box-shadow p-5 text-decoration-none"  style="background-image: url(img/homepages/theme-presentation/account.png);">
-            <h3 class="h5">Online Accounting</h3>
-            <p class="text-muted pb-2">See your financials online anytime, anywhere on your Mac, PC, tablet or mobile phone. No limits</p>
+            <h3 class="h5">{{ __('Online Accounting') }}</h3>
+            <p class="text-muted pb-2">{{ __('See your financials online anytime, anywhere on your Mac, PC, tablet or mobile phone. No limits') }}</p>
             </a></div>
 
             <div class="col-lg-4 col-sm-6 mb-30"><a class="d-block bg-right-center bg-no-repeat bg-white border box-shadow p-5 text-decoration-none"  style="background-image: url(img/homepages/theme-presentation/docs.png);">
-            <h3 class="h5">Deposits & Transfers</h3>
-            <p class="text-muted pb-2">Add deposits to and transfers between accounts and keep the balance of your bank accounts active.</p>
+            <h3 class="h5">{{ __('Deposits & Transfers') }}</h3>
+            <p class="text-muted pb-2">{{ __('Add deposits to and transfers between accounts and keep the balance of your bank accounts active.') }}</p>
             </a>
         </div>
       </div>
