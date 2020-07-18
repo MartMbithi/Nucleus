da36516bbe92f26a947ff1fe525b9d257ede4c6d
martin <martdevelopers254@gmail.com>
2020-07-18 12:42:40 +0300

    Implemented NucleusERP Bootstrapping

---
+++ resources/views/n/bootstrap/autoload.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @package     MartDevelopers Nucleus ERP
+ * @copyright   2019. All rights reserved.
+ * @license     GNU GPL version 3; see LICENSE.txt
+ * @link        https://martmbithi.github.io, https://martdev.info
+ */
+
+define('LARAVEL_START', microtime(true));
+
+/*
+|--------------------------------------------------------------------------
+| Register The Composer Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader
+| for our application. We just need to utilize it! We'll require it
+| into the script here so that we do not have to worry about the
+| loading of any our classes "manually". Feels great to relax.
+|
+*/
+
+require __DIR__.'/../vendor/autoload.php';
+
+// Load composer for modules
+foreach (glob(__DIR__ . '/../modules/*') as $folder) {
+    $autoload = $folder . '/vendor/autoload.php';
+
+    if (!is_file($autoload)) {
+        continue;
+    }
+
+    require $autoload;
+}
---
+++ resources/views/n/index.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package     MartDevelopers Nucleus ERP
+ * @copyright   2019. All rights reserved.
+ * @license     GNU GPL version 3; see LICENSE.txt
+ * @link        https://martmbithi.github.io, https://martdev.info
+ */
+
+// Define minimum supported PHP version
+define('NucleusERP_PHP', '5.6.4');
+
+// Check PHP version
+if (version_compare(PHP_VERSION, NucleusERP_PHP, '<')) {
+    die('Your host needs to use PHP ' . NucleusERP_PHP . ' or higher to run NucleusERP');
+}
+
+// Register the auto-loader
+require(__DIR__.'/bootstrap/autoload.php');
+
+// Load the app
+$app = require_once(__DIR__.'/bootstrap/app.php');
+
+// Run the app
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
